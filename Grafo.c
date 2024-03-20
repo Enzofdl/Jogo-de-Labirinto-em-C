02b728e6862cd1b430a6d319f295112cbdbce5f4
caiudi <127546034+caiudi@users.noreply.github.com>
2024-03-20 11:12:57 -0300

    Update Grafo.c

--- Grafo.c
+++ Grafo.c
@@ -2,5 +2,23 @@
 #include <stdlib.h>
 #include "Grafo.h"
 
+Grafo *criaGrafo(int v)
+{
+    Grafo *g = (Grafo *)malloc(sizeof(Grafo));
+    g->vertices = v;
+    g->arestas = 0;
+    g->adj = (Vertice *)malloc(v*sizeof(Vertice));
+
+    for(int i = 0 ; i < v ; i++)
+    {
+        g->adj[i].head=NULL;
+    }
+    return(g);
+}
+
+Adjacencia *criaAdjacencia(int v)
+
+bool criaAresta(Grafo *gr, int vi, int vf)
+
 
 
