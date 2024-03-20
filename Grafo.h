db2b2f0e15c7896a8d51aa78450cab8f609a178a
caiudi <127546034+caiudi@users.noreply.github.com>
2024-03-20 11:08:11 -0300

    Update Grafo.h

--- Grafo.h
+++ Grafo.h
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#typedef int bool
 
+typedef struct adjacencia
+{
+    int vertice;
+    struct adjacencia *prox;
+}Adjacencia;
+
+typedef struct vertice
+{
+    Adjacencia *head;
+}Vertice;
+
+typedef struct grafo
+{
+    int vertices;
+    int arestas;
+    Vertice *adj;
+}Grafo;
+
+Grafo *criaGrafo(int v);
+Adjacencia *criaAdjacencia(int v);
+bool criaAresta(Grafo *gr, int vi, int vf);
